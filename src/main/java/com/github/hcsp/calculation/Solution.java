cd2fc0cfeaa1a6e6298ec6961478650dec9becba
左佳宾 <59503711+keda725@users.noreply.github.com>
2020-06-06 19:21:41 +0800

    解一元二次方程组 (#195)
    解一元二次方程组 (#195)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -3,6 +3,9 @@ package com.github.hcsp.calculation;
 import java.util.Arrays;
 
 public class Solution {
+
+    public static final double DOUBLE = 0.0001;
+
     public static void main(String[] args) {
         System.out.println("x^2-2x+1=0的解是：" + Arrays.toString(calculate(1, -2, 1)));
         System.out.println("x^2-4=0的解是：" + Arrays.toString(calculate(1, 0, -4)));
@@ -15,5 +18,20 @@ public class Solution {
     // 若没有根，返回一个空数组，即：new double[] {}
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
-    public static double[] calculate(int a, int b, int c) {}
+    @SuppressWarnings("AlibabaAvoidComplexCondition")
+    public static double[] calculate(int a, int b, int c) {
+        double param = b * b -4 * a * c;
+        if (param < 0) {
+            return new double[]{};
+        } else {
+            double root1 = ((-b + Math.sqrt(param)) * 1) / (2 * a);
+            double root2 = ((-b - Math.sqrt(param)) * 1) / (2 * a);;
+            if (root1 > 0 && root2 > 0 || root1 < 0 && root2 < 0 ) {
+                if (Math.abs(root1)-Math.abs(root2) < DOUBLE) {
+                    return new double[] {root1};
+                }
+            }
+            return new double[] {root1,root2};
+        }
+    }
 }
