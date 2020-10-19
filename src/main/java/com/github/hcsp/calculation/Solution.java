4c0179da9ea10cb6c26925fd649e399c4e1a0f90
sgz886 <sgz886@gmail.com>
2020-10-19 16:34:41 +0800

    一元二次方程 (#214)
    一元二次方程 (#214)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -15,5 +15,14 @@ public class Solution {
     // 若没有根，返回一个空数组，即：new double[] {}
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
-    public static double[] calculate(int a, int b, int c) {}
+    public static double[] calculate(int a, int b, int c) {
+        Double delta = Math.sqrt(b * b - 4 * a * c);
+        if (Double.isNaN(delta)) {
+            return new double[]{};
+        } else if (delta.equals(0.0)) {
+            return new double[]{-b / 2.0 / a};
+        } else {
+            return new double[]{(-b + delta) / 2.0 / a, (-b - delta) / 2.0 / a};
+        }
+    }
 }
