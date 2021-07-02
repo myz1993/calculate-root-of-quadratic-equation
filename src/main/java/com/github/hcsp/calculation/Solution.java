bca2b0601a394ffa64ee5edaa4234c87baa89da6
xiaohu li <57934973+lxh6688@users.noreply.github.com>
2021-07-03 00:41:30 +0800

    求方程的根 (#240)
    求方程的根 (#240)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -15,5 +15,16 @@ public class Solution {
     // 若没有根，返回一个空数组，即：new double[] {}
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
-    public static double[] calculate(int a, int b, int c) {}
+    public static double[] calculate(int a, int b, int c) {
+        double result = b * b - 4 * a * c;
+        if (result < 0) {
+            return new double[]{};
+        }else if(Math.abs(result) < 0.000001){
+            return new double[]{-b / 2};
+        }else{
+            double rootA = (-b - Math.sqrt(result)) / 2;
+            double rootB = (-b + Math.sqrt(result)) / 2;
+            return new double[]{rootA, rootB};
+        }
+    }
 }
