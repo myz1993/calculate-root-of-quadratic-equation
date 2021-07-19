ee4f10394f09cd2f4cd94c8efeb10462872ffa29
simonsyang <75574231+Vcsimonsyang@users.noreply.github.com>
2021-07-19 09:43:22 +0800

    output the root of equation of two degree (#242)
    output the root of equation of two degree (#242)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -15,5 +15,17 @@ public class Solution {
     // 若没有根，返回一个空数组，即：new double[] {}
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
-    public static double[] calculate(int a, int b, int c) {}
+    public static double[] calculate(int a, int b, int c) {
+        double n = Math.pow(b, 2) - 4 * a * c;
+        if (n > 0) {
+            double[] root = new double[2];
+            root[0] = (Math.sqrt(n) - b) / (2 * a);
+            root[1] = (-Math.sqrt(n) - b) / (2 * a);
+            return root;
+        }else if( n == 0){
+            return new double[]{(Math.sqrt(n) - b) / (2 * a)};
+        }else{
+            return new double[]{};
+        }
+    }
 }
